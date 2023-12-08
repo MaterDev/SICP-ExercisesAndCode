5d7eee99ea089bde50c8479560478c2261eaed60
Key Clark <aryeh.the.lion@gmail.com>
2023-12-08 03:36:39 -0600

    create example

---
+++ example.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+
+;;; This is an example file, using the rkt extension, which is for the Racket dialect of LISP, using sicp.
+
+(define (print-mouse)
+  (display "      /\\___/\\ \n")
+  (display "     (  o o  ) \n")
+  (display "      \\  >#< / \n")
+  (display "       /     \\ \n")
+  (display "      /       \\ \n")
+  (display "      \\       / \n")
+  (display " ____/______\\__ \n")
+  (display "      /\\   /\\ \n")
+  (display "     /  \\ /  \\ \n"))
+
+(print-mouse)
