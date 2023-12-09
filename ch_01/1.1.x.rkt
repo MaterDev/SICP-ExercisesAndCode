530ae9a0a46af17ef26d1f04e21057e10bfec3b0
Key Clark <aryeh.the.lion@gmail.com>
2023-12-09 09:49:57 -0600

    update 1.1.x notes

--- ch_01/1.1.x.rkt
+++ ch_01/1.1.x.rkt
@@ -1,6 +1,7 @@
 #lang sicp
 
 ;;; Section 1.1
+
 ;;; A base 10 number as an expression
 33
 ;;; Primative procedures
@@ -24,7 +25,43 @@
    (+ (- 10 7)
       6))
 
+;; Section 1.1.2
+
 ;; Naming
 (define size 2)
 size
 (* 5 size)
+
+(define pi 3.14159)
+(define radius 10)
+(* pi (* radius radius))
+(define circumfrence (* 2 pi radius))
+circumfrence
+
+;; Section 1.1.3
+
+;; Evaluating Combinations
+(* (+ 2 (* 4 6 ))
+   (+ 3 5 7)) ;; This requires that evaluation rules be applied to four different combinations
+
+;; Section 1.1.4
+
+;; Compound Procedures
+(define (square x) (* x x))
+(square 3)
+(square (+ 2 2))
+(square (square 3)) ;; equivalent to (3*3) * (3*3)
+
+;; Using procedures to define other procedures
+;; Sum of squares (x^2 + y^2
+(define (sum-of-squares x y)
+  (+ (square x)
+     (square y)))
+
+(sum-of-squares 3 4)
+
+;; Using sum-of-squares to construct further procedures
+(define (f a)
+  (sum-of-squares (+ a 1)
+                   (* a 2)))
+(f 5)
