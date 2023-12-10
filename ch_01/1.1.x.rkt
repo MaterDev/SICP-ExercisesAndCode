b6f76e16f258a7ddf056ee26ca64c8e2f650fd56
Key Clark <aryeh.the.lion@gmail.com>
2023-12-09 22:09:41 -0600

    update notes for 1.1.x

--- ch_01/1.1.x.rkt
+++ ch_01/1.1.x.rkt
@@ -53,7 +53,7 @@ circumfrence
 (square (square 3)) ;; equivalent to (3*3) * (3*3)
 
 ;; Using procedures to define other procedures
-;; Sum of squares (x^2 + y^2
+;; Sum of squares (x^2 + y^2)
 (define (sum-of-squares x y)
   (+ (square x)
      (square y)))
@@ -65,3 +65,41 @@ circumfrence
   (sum-of-squares (+ a 1)
                    (* a 2)))
 (f 5)
+
+;; Section 1.1.6
+
+;; Conditional expressions
+;; Get the absolute value of x
+(define (abs x)
+  (cond ((> x 0) x) ;; If x is greater than 0, return x
+        ((= x 0) 0) ;; If x is 0, return 0
+        ((< x 0) (- x)))) ;; If x is less than zero (negative number), return -x
+(abs -10)
+
+;; Alternate absolute-value procedure (with else)
+(define (absAltElse x)
+  (cond ((< x 0) (- x))
+        (else x)))
+(absAltElse -5)
+
+;; Another absolute-value procedure (with special form of if)
+(define (absAltIf x)
+  (if (< x 0) ;; if x is less than 0, return 0; otherwise return x
+      (- x)
+      x))
+(absAltIf -3)
+
+;; If one number is greater than or equal to another
+(define (>= x y)
+  (or (> x y)
+      (= x y)))
+(>= 4 2) ; true
+(>= 2 2) ; true
+(>= 1 2) ; false
+
+;; Alternative to test if one number is greater than or equal to another
+(define (>=Alt x y)
+  (not (< x y))) ;; if x is less than y, expression will be true an `not` will return false.
+(>=Alt 4 2) ; true
+(>=Alt 2 2) ; true
+(>=Alt 1 2) ; false
